8b5a018c5830e05feac6784b3d4d33f90020cc26
Sergio Ayerdis <72574282+SJAR03@users.noreply.github.com>
2023-06-16 00:09:43 -0600

    Ejemplo 1 de uso de Hooks, useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/pure/greeting'
-import GreetingF from './components/pure/greetingF';
-import Task_listComponent from './components/container/task_list';
+// import Greeting from './components/pure/greeting'
+// import GreetingF from './components/pure/greetingF';
+// import Task_listComponent from './components/container/task_list';
+import Ejemplo1 from './hooks/Ejemplo1';
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
         {/* Componente de ejemplo funcional */}
         {/* <GreetingF name="Juan"></GreetingF> */}
         {/* Componente de listado de tareas */}
-        <Task_listComponent></Task_listComponent>
+        {/* <Task_listComponent></Task_listComponent> */}
+        {/* Ejemplo de uso de HOOKS */}
+        <Ejemplo1></Ejemplo1>
       </header>
     </div>
   );
--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -5,14 +5,18 @@ import TaskComponent from '../pure/task'
 
 const Task_listComponent = () => {
 
-  const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL)
+  const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL);
+
+  // const changeState = (id) => {
+  //   console.log('TODO: Cambiar estado de una tarea')
+  // }
 
   return (
     <div>
       <div>
         <h1>
           Your tasks:
-        </h1> 
+        </h1>
       </div>
       {/* TODO: Aplicar un For/Map para renderizar una lista */}
       <TaskComponent task={defaultTask}></TaskComponent>
